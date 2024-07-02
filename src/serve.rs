165d305bed6f27b459c782580410acb22589cb44
Adrian Tombu <adrian@otso.fr>
2024-07-02 18:09:43 +0000

    Remove flaky hot reload on serve command

--- src/serve.rs
+++ src/serve.rs
@@ -2,20 +2,11 @@ use crate::build::run as run_build;
 use crate::Config;
 use anyhow::{Context, Result};
 use console::style;
-use notify::event::DataChange::Content;
-use notify::event::{RemoveKind, RenameMode};
-use notify::{Event, RecommendedWatcher, RecursiveMode, Watcher};
 use rouille::Response;
-use std::path::{Path, PathBuf};
 
 /// Runs a local server to navigate the blog
 pub fn run() -> Result<()> {
     run_build().context("Failed to build the blog")?;
-
-    let mut watcher = get_watcher()?;
-    watcher.watch(Path::new("posts"), RecursiveMode::Recursive)?;
-    watcher.watch(Path::new("static"), RecursiveMode::Recursive)?;
-    watcher.watch(Path::new("themes"), RecursiveMode::Recursive)?;
 
     println!("{}", style("Listening to http://localhost:1337...").green());
 
@@ -28,50 +19,4 @@ pub fn run() -> Result<()> {
 
         Response::redirect_302("/index.html")
     });
-}
-
-fn get_watcher() -> Result<RecommendedWatcher> {
-    Ok(RecommendedWatcher::new(
-        move |res: Result<Event, notify::Error>| match res {
-            Ok(event) => match event.kind {
-                notify::EventKind::Create(create_kind)
-                    if create_kind == notify::event::CreateKind::File
-                        || create_kind == notify::event::CreateKind::Folder =>
-                {
-                    rebuild(&event.paths, "created");
-                }
-
-                notify::EventKind::Modify(kind)
-                    if kind == notify::event::ModifyKind::Data(Content)
-                        || kind == notify::event::ModifyKind::Name(RenameMode::To) =>
-                {
-                    rebuild(&event.paths, "modified");
-                }
-
-                notify::EventKind::Remove(RemoveKind::Any) => {
-                    rebuild(&event.paths, "deleted");
-                }
-                _ => (),
-            },
-            Err(e) => println!("watch error: {e:?}"),
-        },
-        notify::Config::default(),
-    )?)
-}
-
-fn rebuild(paths: &[PathBuf], action: &str) {
-    if let Some(path) = paths.first() {
-        println!(
-            "{}",
-            style(format!(
-                "{} has been {action}. Reloading...",
-                path.display()
-            ))
-            .yellow()
-        );
-
-        if let Err(e) = run_build() {
-            println!("{}", style(format!("Error building the blog: {e:?}")).red());
-        };
-    }
 }
